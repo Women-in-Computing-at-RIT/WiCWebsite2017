7a45cf4be6a3f524307bf65dabf2853234715794
lad4284 <lad4284@rit.edu>
2017-10-13 16:02:22 -0400

    php includes for header and footer

---
+++ header.php
@@ -0,0 +1,74 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home | Women in Computing</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600&amp;subset=latin-ext" rel="stylesheet">
+    
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <link href="css/ritstyles.css" rel="stylesheet">
+    
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>    
+<header class="site-header">
+        <div class="top">
+            <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="http://www.rit.edu/">
+            <img src="/images/idbar-black.gif" id="rit-logo">
+          </a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="http://www.rit.edu/directories/">Directories</a></li>
+          </ul>
+        </div>
+    </div>
+        </div>
+        <nav class="navbar navbar-default">
+			<div class="container">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse">
+					<span class="sr-only">Toggle Navigation</span>
+					<i class="fa fa-bars"></i>
+				</button>
+				<a href="index.html" class="navbar-brand">
+					<img src="img/logo.png" alt="Post">
+				</a>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-navbar-collapse">
+                    <ul class="nav navbar-nav main-navbar-nav">
+                        <li class="active"><a href="index.html" title="">HOME</a></li>
+                        <li class="dropdown">
+                            <a href="#" title="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ABOUT<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="mission.html" title="">MISSION</a></li>
+                                <li><a href="#" title="">COMMITTEES</a></li>
+                                <li><a href="#" title="">ALLIES</a></li>
+                                <li><a href="#" title="">PROJECTS</a></li>
+                                <li><a href="#" title="">ALUMNI</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" title="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">EVENTS<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#" title="">UPCOMING CALENDAR</a></li>
+                                <li><a href="#" title="">WICHACKS</a></li>
+                                <li><a href="#" title="">ANNUAL EVENTS</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#" title="">AFTER HOURS</a></li>
+                        <li><a href="#" title="">BLOG</a></li>
+                        <li><a href="#" title="">SPONSOR</a></li>
+                        <li><a href="page.html" title="">CONTACT US</a></li>
+                    </ul>                           
+                </div><!-- /.navbar-collapse -->                
+				<!-- END MAIN NAVIGATION -->
+			</div>
+		</nav>        
+    </header>
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <?php include 'header.php';?>
+    <main class="site-main">
+        <section class="hero_area">
+            <div class="hero_content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h1>Women in Computing at RIT</h1>
+                            <h2>Dedicated to promoting the success and advancement of women in their academic and professional careers</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="boxes_area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="box">
+                            <h3>FALL  MEETING TIMES</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id pulvinar magna. Aenean accumsan iaculis lorem, nec sodales lectus auctor tempor.</p>
+                            <i class="fa fa-clock-o"></i>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="box">
+                            <h3>UPCOMING EVENTS</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id pulvinar magna. Aenean accumsan iaculis lorem, nec sodales lectus auctor tempor.</p>
+                            <i class="fa fa-calendar"></i>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="box">
+                            <h3>FEATURED NEWS</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id pulvinar magna. Aenean accumsan iaculis lorem, nec sodales lectus auctor tempor.</p>
+                            <i class="fa fa-newspaper-o"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+    </main>
+    <?php include 'footer.php';?>
+</body>
+</html>
