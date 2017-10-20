b9de4edf0c978c375cb7ee0fe15c38b1edd39ab4
lad4284 <lad4284@rit.edu>
2017-10-20 16:14:06 -0400

    added wichacks.php

---
+++ wichacks.php
@@ -0,0 +1,50 @@
+<?php $title="WiCHacks"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<body>
+     <?php include 'header.php';?>
+    <div class="bread_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <ol class="breadcrumb">
+                        <li><a href="#">Home</a></li>
+                        <li class="active">About</li>
+                    </ol>                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <main class="site-main page-main">
+        <div class="container">
+            <div class="row">
+                <section class="page col-sm-9">
+                    <h2 class="page-title">WiCHacks: Women in Computing's Annual Hackaton</h2>
+                    <div class="entry">
+                        <p>WiCHacks is an all women 24-hour hackathon hosted by Women in Computing at RIT. A hackathon is a collaborative programming event in which participants create an app, website, game, or other piece of software over the course of the event.</p>
+                        
+                        
+                        <div class="wichackscontent">
+                           
+                                <img src="img/wichackslogo.png" alt="wichacks logo"/><br>
+                                <iframe width="560px" height="315px" src="https://www.youtube.com/embed/OxlXmc-jViI?rel=0"
+                                    frameborder="0" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </section>
+                <aside class="sidebar col-sm-3">
+                    <div class="widget">
+                        <h4>EVENTS</h4>
+                        <ul>
+                            <li><a href="#" title="">Upcoming Calendar</a></li>
+                            <li class="current"><a href="#" title="">WiCHacks</a></li>
+                            <li><a href="#" title="">Annual Events</a></li>
+                        </ul>
+                    </div>
+                </aside>
+            </div>
+        </div>
+    </main>
+     <?php include 'footer.php';?>
+</body>
+</html>
