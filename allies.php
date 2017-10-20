ff9ff6cc4a431e1ff93cc660e193b39b186005ce
Ariel <aviggiano836@gmail.com>
2017-10-20 17:59:16 -0400

    removed extra header in allies.php

--- allies.php
+++ allies.php
@@ -2,23 +2,6 @@
 <?php $title="Allies" ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Allies | Women in Computing at RIT</title>
-    
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600&amp;subset=latin-ext" rel="stylesheet">
-    
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="style.css" rel="stylesheet">
-    
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
 <?php include 'header.php';?>
 <body>
     <div class="bread_area">
