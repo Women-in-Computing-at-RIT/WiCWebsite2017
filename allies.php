a928de8d3e0dd3269da60e3a3bf29f1971d7336f
dkrsorenson <dkrsorenson@gmail.com>
2017-10-20 16:13:19 -0400

    Allies and Committees Updates
    Changed content in allies and added temp pictures. Added pictures to committees.

--- allies.php
+++ allies.php
@@ -19,64 +19,15 @@
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
+<?php include 'header.php';?>
 <body>
-    <header class="site-header">
-        <div class="top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <p>Lorem Ipsum Dolor Sit Amet</p>
-                    </div>
-                    <div class="col-sm-6">
-                        <ul class="list-inline pull-right">
-                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                            <li><a href="#"><i class="fa fa-envelope-o"></i></a></li>
-                            <li><a href="tel:+902222222222"><i class="fa fa-phone"></i> +90 222 222 22 22</a></li>
-                        </ul>                        
-                    </div>
-                </div>
-            </div>
-        </div>
-        <nav class="navbar navbar-default">
-			<div class="container">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse">
-					<span class="sr-only">Toggle Navigation</span>
-					<i class="fa fa-bars"></i>
-				</button>
-				<a href="index.html" class="navbar-brand">
-					<img src="img/logo.png" alt="Post">
-				</a>
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-navbar-collapse">
-                    <ul class="nav navbar-nav main-navbar-nav">
-                        <li><a href="index.html" title="">HOME</a></li>
-                        <li class="dropdown">
-                            <a href="#" title="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">DROPDOWN MENU <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#" title="">SUB MENU 1</a></li>
-                                <li><a href="#" title="">SUB MENU 2</a></li>
-                                <li><a href="#" title="">SUB MENU 3</a></li>
-                            </ul>
-                        </li>
-                        <li class="active"><a href="page.html" title="">PAGE</a></li>
-                        <li><a href="category.html" title="">CATEGORY</a></li>
-                        <li><a href="#" title="">MENU ITEM</a></li>
-                        <li><a href="#" title="">MENU ITEM</a></li>
-                    </ul>                           
-                </div><!-- /.navbar-collapse -->                
-				<!-- END MAIN NAVIGATION -->
-			</div>
-		</nav>        
-    </header>
     <div class="bread_area">
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
                     <ol class="breadcrumb">
-                        <li><a href="#">Home</a></li>
-                        <li class="active">About</li>
+                        <li><a href="http://localhost/index.php">Home</a></li>
+                        <li class="active">Allies</li>
                     </ol>                    
                 </div>
             </div>
@@ -86,104 +37,55 @@
         <div class="container">
             <div class="row">
                 <section class="page col-sm-9">
-                    <h2 class="page-title">MISSION STATEMENT</h2>
+                    <h2 class="page-title">ALLIES</h2>
                     <div class="entry">
-                        <p>Women in Computing at the Rochester Institute of Technology (WiC at RIT) provides an interdisciplinary networking forum for students, faculty, staff and alumni involved in the field of computing at The Rochester Institute of Technology through professional, leadership, mentorship, and technical development opportunities. WiC at RIT will also encourage the advancement of women in computing through community outreach.</p>
-                        <p style="text-align:center"><b><u>2017 -2018 Constitution </u></b></p>
+                        <p>WiC Allies is a group of RIT and GCCIS community members committed to furthering the mission and goals of Women in Computing, for the purpose of affecting positive change and fostering a more inclusive GCCIS culture.</p>
+<!--                        <p style="text-align:center"><b><u>2017 -2018 Constitution </u></b></p>-->
                     </div>
                     <br><br>
-                    <h2 class="page-title">WHO WE ARE</h2>
+<!--
+                    <h2 class="page-title">CURRENT GOALS</h2>
                     <div class="entry">
                         <p>Women in Computing at Rochester Institute of Technology is dedicated to promoting the success and advancement of women in their academic and professional careers. Lana Verschage, the Director of Women in Computing, works with all WiC Committee Leaders, active members and members. Our ultimate goal is to empower women in computer fields to succeed and thrive at Rochester Institute of Technology and beyond.</p>
                     </div>
                     <br><br>
-                    <h2 class="page-title">GOALS</h2>
+-->
+                    <h2 class="page-title">CURRENT GOALS</h2>
                     <div class="entry">
                         <ul>
-                            <li>To make the field of computing seem less intimidating to women and allow them to become better integrated within it.</li>
-                            <li>To break down gender barriers in the field and promote a truly equal and inclusive environment.</li>
-                            <li>To organize social events to strengthen relationships to encourage those who may feel ostracized.</li>
-                            <li>To encourage young women ages K-12 to explore computing through outreach programs, camps, and mentorship.</li>
-                            <li>To create opportunities for members to gain and expand their technical knowledge through projects, WiCHacks, tech talks, and conferences.</li>
-                            <li>To provide a channel for distributing event information from other organizations within the field.</li>
-                            <li>To accumulate technical news, knowledge, and information for use in encouraging the involvement of women in the field of computing and providing a clearer path to success within it.</li>
+                            <li>Learn how to be a better ally looking at topics:</li>
+                                <ul>
+                                    <li>Teaching bystanders to take action</li>
+                                    <li>Unconscious bias</li>
+                                    <li>Micro Aggressions</li>
+                                </ul>
+                            <li>Volunteering at WiC events</li>
                         </ul>
                     </div>
                     <br><br>
-                    <h2 class="page-title">GET INVOLVED</h2>
+                    <h2 class="page-title">HOW TO BE AN ALLY</h2>
                     <div class="entry">
-                        <p>All students in the Rochester Institute of Technology are eligible for membership in WiC at RIT.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id pulvinar magna. Aenean accumsan iaculis lorem, nec sodales lectus auctor tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id pulvinar magna. Aenean accumsan iaculis lorem, nec sodales lectus auctor tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id pulvinar magna. Aenean accumsan iaculis lorem, nec sodales lectus auctor tempor.</p>
                     </div>
+                    <img class="card-img-top" src="http://placehold.it/350x250" alt="placeholder" width="300" height="200" style="padding-right: 20px" />
+                    <img class="card-img-top" src="http://placehold.it/350x250" alt="placeholder" width="300" height="200" style="padding-left: 20px"/>
                 </section>
                 <aside class="sidebar col-sm-3">
                     <div class="widget">
                         <h4>ABOUT</h4>
                         <ul>
                             <li class="current"><a href="#" title="">Mission</a></li>
-                            <li><a href="#" title="">Committees</a></li>
-                            <li><a href="#" title="">Allies</a></li>
-                            <li><a href="#" title="">Projects</a></li>
-                            <li><a href="#" title="">Alumni</a></li>
+                            <li><a href="http://wic.rit.edu/committees.php" title="Committees">Committees</a></li>
+                            <li><a href="http://wic.rit.edu/allies.php" title="Allies">Allies</a></li>
+                            <li><a href="http://wic.rit.edu/projects.php" title="Projects">Projects</a></li>
+                            <li><a href="http://wic.rit.edu/alumni.php" title="Alumni">Alumni</a></li>
                         </ul>
                     </div>
                 </aside>
             </div>
         </div>
     </main>
-    <footer class="site-footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 col-sm-6 fbox">
-                    <h4>COMPANY NAME</h4>
-                    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam congue lectus diam, sit amet cursus massa efficitur sed. </p>
-                    <ul class="list-inline">
-                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>                        
-                    </ul>
-                </div>
-
-                <div class="col-md-3 col-sm-6 fbox">
-                    <h4>CONTENT</h4>
-                    <ul class="big">
-                        <li><a href="#" title="">Title One</a></li>
-                        <li><a href="#" title="">Title Two</a></li>
-                        <li><a href="#" title="">Title Three</a></li>
-                        <li><a href="#" title="">Title Four</a></li>
-                        <li><a href="#" title="">Title Five</a></li>
-                        <li><a href="#" title="">Title Six</a></li>
-                        <li><a href="#" title="">Title Seven</a></li>
-                        <li><a href="#" title="">Title Eight</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-3 col-sm-6 fbox">
-                    <h4>CONTENT</h4>
-                    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    <p><a href="tel:+902222222222"><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> +90 222 222 22 22</a></p>
-                    <p><a href="mailto:iletisim@agrisosgb.com"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> mail@awebsitename.com</a></p>
-                </div>
-            </div>
-        </div>
-        <div id="copyright">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4">
-                        <p class="pull-left">&copy; 2016 COMPANY NAME</p>
-                    </div>
-                    <div class="col-md-8">
-                        <ul class="list-inline navbar-right">
-                            <li><a href="#">HOME</a></li>
-                            <li><a href="#">MENU ITEM</a></li>
-                            <li><a href="#">MENU ITEM</a></li>
-                            <li><a href="#">MENU ITEM</a></li>
-                            <li><a href="#">MENU ITEM</a></li>
-                            <li><a href="#">MENU ITEM</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>        
-    </footer>
+    <?php include 'footer.php';?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </body>
