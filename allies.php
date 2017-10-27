9c6bcfe3bd4b84332b8da17fbe4d99474e11a34f
lad4284 <lad4284@rit.edu>
2017-10-27 15:16:50 -0400

    fixed the thing

---
+++ allies.php
@@ -0,0 +1,74 @@
+<!--Allies WiC Page-->
+<?php $title="Allies" ?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'header.php';?>
+<body>
+    <div class="bread_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <ol class="breadcrumb">
+                        <li><a href="index.php">Home</a></li>
+                        <li>About</li>
+                        <li class="active">Allies</li>
+                    </ol>                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <main class="site-main page-main">
+        <div class="container">
+            <div class="row">
+                <section class="page col-sm-9">
+                    <h2 class="page-title">ALLIES</h2>
+                    <div class="entry">
+                        <p>WiC Allies is a group of RIT and GCCIS community members committed to furthering the mission and goals of Women in Computing, for the purpose of affecting positive change and fostering a more inclusive GCCIS culture.</p>
+                        
+                        <div style="text-align:center">
+                            <img class="alliespics" src="http://placehold.it/300x200" alt="placeholder" style="padding: 10px; padding-bottom:0"/>
+                            <img class="alliespics" src="http://placehold.it/300x200" alt="placeholder" style="padding: 10px; padding-bottom:0"/>
+                        </div>
+                    </div>
+                    
+                    <br><br>
+
+                    <h2 class="page-title">CURRENT GOALS</h2>
+                    <div class="entry">
+                        <ul>
+                            <li>Learn how to be a better ally looking at topics:</li>
+                                <ul>
+                                    <li>Teaching bystanders to take action</li>
+                                    <li>Unconscious bias</li>
+                                    <li>Micro Aggressions</li>
+                                </ul>
+                            <li>Volunteering at WiC events</li>
+                        </ul>
+                    </div>
+                    <br><br>
+                    <h2 class="page-title">HOW TO BE AN ALLY</h2>
+                    <div class="entry">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id pulvinar magna. Aenean accumsan iaculis lorem, nec sodales lectus auctor tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id pulvinar magna. Aenean accumsan iaculis lorem, nec sodales lectus auctor tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id pulvinar magna. Aenean accumsan iaculis lorem, nec sodales lectus auctor tempor.</p>
+                    </div>
+
+                </section>
+                <aside class="sidebar col-sm-3">
+                    <div class="widget">
+                        <h4>ABOUT</h4>
+                        <ul>
+                            <li><a href="mission.php" title="">Mission</a></li>
+                            <li><a href="committees.php" title="Committees">Committees</a></li>
+                            <li class="current"><a href="allies.php" title="Allies">Allies</a></li>
+                            <li><a href="projects.php" title="Projects">Projects</a></li>
+                            <li><a href="alumni.php" title="Alumni">Alumni</a></li>
+                        </ul>
+                    </div>
+                </aside>
+            </div>
+        </div>
+    </main>
+    <?php include 'footer.php';?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,112 @@
+<?php $title="Home"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <?php include 'header.php';?>
+    <main class="site-main">
+        <section class="hero_area">
+            <div class="hero_content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h1>Women in Computing at RIT</h1>
+                            <h2>Dedicated to promoting the success and advancement of women in their academic and professional careers</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="boxes_area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="box" id="meetingtimes">
+                            <h3>FALL  MEETING TIMES</h3>
+                                <b>General Meeting:</b>
+                                <ul>
+                                    <li>Wed • 6:30pm • SIH-1600</li>
+                                    <li>(The Fishbowl- unless otherwise stated)</li>
+                                </ul>
+                                <b>Events Committee:</b>
+                                <ul>
+                                    <li>Fri • 2:00pm • GOL-2600</li>
+                                </ul>
+                                <b>Outreach Committee:</b>
+                                <ul>
+                                    <li>Mon • 4:35pm • GOL-2600</li>
+                                </ul>
+                                <b>Public Relations Committee:</b>
+                                <ul>
+                                    <li>Mon • 6:30pm • INS-1080</li>
+                                </ul>
+                                <b>WiC Allies:</b>
+                                <ul>
+                                    <li>Wed • 12:00pm • GOL-2600</li>
+                                </ul>
+                                <b>WiCHacks Committee:</b>
+                                <ul>
+                                    <li>Wed • 8:00pm • GOL-3560</li>
+                                </ul>
+                                <b>Projects Committee:</b>
+                                <ul>
+                                    <li>Mon • 5:35pm • GOL-2600</li>
+                                </ul>
+                            <i class="fa fa-clock-o"></i>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="box" id="meetingtimes">
+                            <h3>UPCOMING EVENTS</h3>
+                            
+                            <b>Volunteer for Girl Scouts Badge Day! </b>
+                                <ul>
+                                    <li>11/4 (9:30am - 4pm)</li>
+                                    <li><a href="">http://bit.ly/GirlScoutsBadgeDay</a></li>
+                                </ul>
+                            <b>RSVP for Google Personal Branding Workshop</b>
+                                <ul>
+                                    <li>10/18 (6-8pm)</li>
+                                    <li><a href="">http://bit.ly/GoogleBrandingWorkshop</a></li>
+                                </ul>
+                            <b>RSVP for Trip to Powers Farm Market</b>
+                                <ul>
+                                    <li>10/22 (2-4pm)</li>
+                                    <li><a href="">http://bit.ly/wic-powers</a></li>
+                                </ul>
+                            <b>Volunteer for ROCGirlHacks</b>
+                                <ul>
+                                    <li>10/28 (9am - 7pm)</li>
+                                    <li><a href="">http://bit.ly/VolunteerROCGirlHacks</a></li>
+                                </ul>
+                            <i class="fa fa-calendar"></i>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="box">
+                            <h3>FEATURED NEWS</h3>
+                            <p><a target="_blank" href="http://www.democratandchronicle.com/story/news/2017/10/27/girl-hackathon-rit/804609001/">
+                                Democrat and Chronicle: All-girl hackathon to be held at RIT to spur interest in STEM careers 
+                            <p><a target="_blank" href="http://www.rit.edu/news/story.php?id=64018&source=enewsletter">
+                                University News: ROCGirlHacks is Oct. 28</a></p>
+                            <p><a target="_blank" href="http://www.rit.edu/news/photoarchive.php?view=photo&datePhoto=2017-01-31">
+                        Campus Spotlight: WiC and the Girl Scouts</a></p>
+                            <p><a target="_blank" href="http://rbj.net/2017/04/20/founder-of-girls-who-code-to-speak-at-rit/">
+                        Founder of Girls Who Code to speak at RIT </a></p>
+                            <p><a target="_blank" href="http://www.rit.edu/news/story.php?id=61929&source=enewsletter">
+                            Girl Scouts honor WiC for promoting girls in STEM</a></p>
+                            <p><a target="_blank" href="https://www.youtube.com/watch?v=OqUx7HMd4zY">WiC on TV: Good Day Rochester</a></p>
+                            <p><a target="_blank" href="http://www.rit.edu/news/story.php?id=59631&source=enewsletter">
+                                University News: WiC Hacks 2017</a></p>
+                            <p><a target="_blank" href="https://www.youtube.com/watch?v=AS9TWebVQ98">WiC on TV: STEM Fair 2017 - ABC 13</a></p>
+                            <p><a target="_blank" href="https://www.youtube.com/watch?v=KEoWd_4Exag">WiC on TV: STEM Fair 2017 - TWC News</a></p>
+                            <i class="fa fa-newspaper-o"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+    </main>
+    <?php include 'footer.php';?>
+</body>
+</html>
