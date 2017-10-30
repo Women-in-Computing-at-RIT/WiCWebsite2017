d5c975464d7f6d8ea3e51fd0293e28b23e6dc33e
lad4284 <lad4284@rit.edu>
2017-10-30 19:23:22 -0400

    basic blog

--- blog.php
+++ blog.php
@@ -16,19 +16,229 @@
             </div>
         </div>
     </div>
-    <main class="site-main page-main">
+    <main class="site-main category-main">
         <div class="container">
             <div class="row">
-                <section class="page col-sm-12">
-                    <h2 class="page-title">BLOG</h2>
-                    <div class="entry">
-                        
-                    </div>
+                <section class="category-content col-sm-9">
+                    <h2 class="category-title">CATEGORY NAME</h2>
+                    <ul class="media-list">
+                        <li class="media">
+                            <div class="media-left">
+                                <a href="#" title="Post">
+                                    <img class="media-object" src="http://via.placeholder.com/262x200" alt="Post">
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                <h3 class="media-heading"><a href="#" title="Post Title">Post Title</a></h3>
+                                <p>Aenean vitae dolor sed purus tempus ullamcorper. Integer urna orci, lacinia ut ornare sit amet, luctus quis est. Ut interdum lorem in mattis lobortis. Maecenas tincidunt justo a lobortis facilisis.</p>
+                                <aside class="meta category-meta">
+                                    <div class="pull-left">
+                                        <div class="arc-comment"><a href="#" title="Comment"><i class="fa fa-comments"></i> 0 Comment</a></div>
+                                        <div class="arc-date">10/15/2016</div>
+                                    </div>
+                                    <div class="pull-right">
+                                        <ul class="arc-share">
+                                            <li><a href="#" title="Post"><i class="fa fa-facebook"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-twitter"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-linkedin"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-google-plus"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </aside>                                
+                            </div>
+                        </li>
+                        <li class="media">
+                            <div class="media-left">
+                                <a href="#" title="Post">
+                                    <img class="media-object" src="http://via.placeholder.com/262x200" alt="Post">
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                <h3 class="media-heading"><a href="#" title="Post Title">Post Title</a></h3>
+                                <p>Aenean vitae dolor sed purus tempus ullamcorper. Integer urna orci, lacinia ut ornare sit amet, luctus quis est. Ut interdum lorem in mattis lobortis. Maecenas tincidunt justo a lobortis facilisis.</p>
+                                <aside class="meta category-meta">
+                                    <div class="pull-left">
+                                        <div class="arc-comment"><a href="#" title="Comment"><i class="fa fa-comments"></i> 1 Comment</a></div>
+                                        <div class="arc-date">10/15/2016</div>
+                                    </div>
+                                    <div class="pull-right">
+                                        <ul class="arc-share">
+                                            <li><a href="#" title="Post"><i class="fa fa-facebook"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-twitter"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-linkedin"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-google-plus"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </aside>                                
+                            </div>
+                        </li>
+                        <li class="media">
+                            <div class="media-left">
+                                <a href="#" title="Post">
+                                    <img class="media-object" src="http://via.placeholder.com/262x200" alt="Post">
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                <h3 class="media-heading"><a href="#" title="Post Title">Post Title</a></h3>
+                                <p>Aenean vitae dolor sed purus tempus ullamcorper. Integer urna orci, lacinia ut ornare sit amet, luctus quis est. Ut interdum lorem in mattis lobortis. Maecenas tincidunt justo a lobortis facilisis.</p>
+                                <aside class="meta category-meta">
+                                    <div class="pull-left">
+                                        <div class="arc-comment"><a href="#" title="Comment"><i class="fa fa-comments"></i> 2 Comments</a></div>
+                                        <div class="arc-date">10/15/2016</div>
+                                    </div>
+                                    <div class="pull-right">
+                                        <ul class="arc-share">
+                                            <li><a href="#" title="Post"><i class="fa fa-facebook"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-twitter"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-linkedin"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-google-plus"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </aside>                                
+                            </div>
+                        </li>
+                        <li class="media">
+                            <div class="media-left">
+                                <a href="#" title="Post">
+                                    <img class="media-object" src="http://via.placeholder.com/262x200" alt="Post">
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                <h3 class="media-heading"><a href="#" title="Post Title">Post Title</a></h3>
+                                <p>Aenean vitae dolor sed purus tempus ullamcorper. Integer urna orci, lacinia ut ornare sit amet, luctus quis est. Ut interdum lorem in mattis lobortis. Maecenas tincidunt justo a lobortis facilisis.</p>
+                                <aside class="meta category-meta">
+                                    <div class="pull-left">
+                                        <div class="arc-comment"><a href="#" title="Comment"><i class="fa fa-comments"></i> 2 Comments</a></div>
+                                        <div class="arc-date">10/15/2016</div>
+                                    </div>
+                                    <div class="pull-right">
+                                        <ul class="arc-share">
+                                            <li><a href="#" title="Post"><i class="fa fa-facebook"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-twitter"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-linkedin"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-google-plus"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </aside>                                
+                            </div>
+                        </li>
+                        <li class="media">
+                            <div class="media-left">
+                                <a href="#" title="Post">
+                                    <img class="media-object" src="http://via.placeholder.com/262x200" alt="Post">
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                <h3 class="media-heading"><a href="#" title="Post Title">Post Title</a></h3>
+                                <p>Aenean vitae dolor sed purus tempus ullamcorper. Integer urna orci, lacinia ut ornare sit amet, luctus quis est. Ut interdum lorem in mattis lobortis. Maecenas tincidunt justo a lobortis facilisis.</p>
+                                <aside class="meta category-meta">
+                                    <div class="pull-left">
+                                        <div class="arc-comment"><a href="#" title="Comment"><i class="fa fa-comments"></i> 12 Comments</a></div>
+                                        <div class="arc-date">10/15/2016</div>
+                                    </div>
+                                    <div class="pull-right">
+                                        <ul class="arc-share">
+                                            <li><a href="#" title="Post"><i class="fa fa-facebook"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-twitter"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-linkedin"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-google-plus"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </aside>                                
+                            </div>
+                        </li>
+                        <li class="media">
+                            <div class="media-left">
+                                <a href="#" title="Post">
+                                    <img class="media-object" src="http://via.placeholder.com/262x200" alt="Post">
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                <h3 class="media-heading"><a href="#" title="Post Title">Post Title</a></h3>
+                                <p>Aenean vitae dolor sed purus tempus ullamcorper. Integer urna orci, lacinia ut ornare sit amet, luctus quis est. Ut interdum lorem in mattis lobortis. Maecenas tincidunt justo a lobortis facilisis.</p>
+                                <aside class="meta category-meta">
+                                    <div class="pull-left">
+                                        <div class="arc-comment"><a href="#" title="Comment"><i class="fa fa-comments"></i> 2 Comments</a></div>
+                                        <div class="arc-date">10/15/2016</div>
+                                    </div>
+                                    <div class="pull-right">
+                                        <ul class="arc-share">
+                                            <li><a href="#" title="Post"><i class="fa fa-facebook"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-twitter"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-linkedin"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-google-plus"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </aside>                                
+                            </div>
+                        </li>
+                        <li class="media">
+                            <div class="media-left">
+                                <a href="#" title="Post">
+                                    <img class="media-object" src="http://via.placeholder.com/262x200" alt="Post">
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                <h3 class="media-heading"><a href="#" title="Post Title">Post Title</a></h3>
+                                <p>Aenean vitae dolor sed purus tempus ullamcorper. Integer urna orci, lacinia ut ornare sit amet, luctus quis est. Ut interdum lorem in mattis lobortis. Maecenas tincidunt justo a lobortis facilisis.</p>
+                                <aside class="meta category-meta">
+                                    <div class="pull-left">
+                                        <div class="arc-comment"><a href="#" title="Comment"><i class="fa fa-comments"></i> 3 Comments</a></div>
+                                        <div class="arc-date">10/15/2016</div>
+                                    </div>
+                                    <div class="pull-right">
+                                        <ul class="arc-share">
+                                            <li><a href="#" title="Post"><i class="fa fa-facebook"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-twitter"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-linkedin"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-google-plus"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </aside>                                
+                            </div>
+                        </li>
+                        <li class="media">
+                            <div class="media-left">
+                                <a href="#" title="Post">
+                                    <img class="media-object" src="http://via.placeholder.com/262x200" alt="Post">
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                <h3 class="media-heading"><a href="#" title="Post Title">Post Title</a></h3>
+                                <p>Aenean vitae dolor sed purus tempus ullamcorper. Integer urna orci, lacinia ut ornare sit amet, luctus quis est. Ut interdum lorem in mattis lobortis. Maecenas tincidunt justo a lobortis facilisis.</p>
+                                <aside class="meta category-meta">
+                                    <div class="pull-left">
+                                        <div class="arc-comment"><a href="#" title="Comment"><i class="fa fa-comments"></i> 8 Comments</a></div>
+                                        <div class="arc-date">10/15/2016</div>
+                                    </div>
+                                    <div class="pull-right">
+                                        <ul class="arc-share">
+                                            <li><a href="#" title="Post"><i class="fa fa-facebook"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-twitter"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-linkedin"></i></a></li>
+                                            <li><a href="#" title="Post"><i class="fa fa-google-plus"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </aside>                                
+                            </div>
+                        </li>                        
+                    </ul>                    
                 </section>
-                
+                <aside class="sidebar col-sm-3">
+                    <div class="widget">
+                        <h4>SERVICES</h4>
+                        <ul>
+                            <li class="current"><a href="#" title="">Service Title One</a></li>
+                            <li><a href="#" title="">Service Title Two</a></li>
+                            <li><a href="#" title="">Service Title Three</a></li>
+                            <li><a href="#" title="">Service Title Four</a></li>
+                        </ul>
+                    </div>
+                </aside>
             </div>
         </div>
     </main>
+
      <?php include 'footer.php';?>
 </body>
 </html>
