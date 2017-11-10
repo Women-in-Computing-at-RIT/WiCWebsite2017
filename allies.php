b47a0cd7814fb937ecd628b8c8eed1964d07f154
dkrsorenson <dkrsorenson@gmail.com>
2017-11-10 16:08:52 -0500

    Updates

--- allies.php
+++ allies.php
@@ -23,13 +23,13 @@
                 <section class="page col-sm-9">
                     <h2 class="page-title">ALLIES</h2>
                     <div class="entry">
-                        <p>WiC Allies is a committee within Women in Computing whose purpose is to help and support the goals of our organization as a whole for the purpose of affecting positive change and fostering a more inclusive GCCIS (Golisano College of Computing and Information Sciences) culture.</p>
+                        <p class="alliesinfo">WiC Allies is a committee within Women in Computing whose purpose is to help and support the goals of our organization as a whole for the purpose of affecting positive change and fostering a more inclusive GCCIS (Golisano College of Computing and Information Sciences) culture.</p>
                         
-                        <p>At the Allies committee meeting, we discuss gender-related issues in computing and help resolve them. Allies also volunteer for WiC events and are members of other WiC committees.</p>
+                        <p class="alliesinfo">At the Allies committee meeting, we discuss gender-related issues in computing and help resolve them. Allies also volunteer for WiC events and are members of other WiC committees.</p>
                             
-                        <p>WiC Allies is not just a committee for men, it is a group that encourages collaboration across all genders to solve gender issues.</p>
+                        <p class="alliesinfo">WiC Allies is not just a committee for men, it is a group that encourages collaboration across all genders to solve gender issues.</p>
 
-                        <p>Join the conversation by attending one of our meetings or workshops.</p>
+                        <p class="alliesinfo">Join the conversation by attending one of our meetings or workshops.</p>
                         
                         <div class="row-alliespics">
                             <img class="alliespics" src="img/allies/allies_larger2.jpg" alt="placeholder"/>
@@ -41,7 +41,7 @@
                     
                     <h2 class="page-title">HOW TO BE AN ALLY</h2>
 
-                    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSRG_OvztNPDq1XNsZ1avpRvQ38jy6do9z-JnQYjM_MpHvJp2g0VDa82Ni9oY_7grLg-CUrzfTR5r5E/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+                    <iframe id="slideshow" src="https://docs.google.com/presentation/d/e/2PACX-1vSRG_OvztNPDq1XNsZ1avpRvQ38jy6do9z-JnQYjM_MpHvJp2g0VDa82Ni9oY_7grLg-CUrzfTR5r5E/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
                     
                 </section>
                 <aside class="sidebar col-sm-3">
