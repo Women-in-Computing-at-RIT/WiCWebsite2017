7e81fd76bfa0ed1bd4cb4e0354de09412efb75c8
dkrsorenson <dkrsorenson@gmail.com>
2017-11-10 16:10:40 -0500

    Fixed mobile nav for allies

--- allies.php
+++ allies.php
@@ -60,7 +60,7 @@
         </div>
     </main>
     <?php include 'footer.php';?>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
+<!--    <script src="js/bootstrap.min.js"></script>-->
 </body>
 </html>
