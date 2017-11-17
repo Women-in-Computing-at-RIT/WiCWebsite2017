e26de46c3b9a203634a825b7a08cc87ddd76c2cb
Rachel Poturich <rxp2553@g.rit.edu>
2017-11-17 15:43:54 -0500

    Annual Events and Style

--- annual_events.php
+++ annual_events.php
@@ -24,62 +24,66 @@
                     <ul class="media-list">
                         <li class="media">
                             <div class="media-left">
-                                <a href="#" title="Post">
-                                    <img class="media-object" src="img/annualevents/wichacks.jpg" alt="Post">
-                                </a>
+                                <img class="media-object" src="img/annualevents/wichacks.jpg" alt="Picture of WiCHacks">
                             </div>
                             <div class="media-body">
-                                <h3 class="media-heading"><a href="#" title="WICHACKS">WICHACKS</a></h3>
-                                <p>WiCHacks is a collaborative programming event in which participants create an app, website, game, or other piece of software over the course of the event. Hackathon participants can be female college or high school students who are at least 16 years of age.</p>                               
+                                <h3 class="media-heading">WiCHacks</h3>
+                                <p>WiCHacks is a collaborative programming event in which participants create an app, website, game, or other piece of software over the course of the event. Hackathon participants can be female college or high school students who are at least 16 years of age. <a href="https://wichacks.io/">Learn More...</a></p>                               
                             </div>
                         </li> 
+                        <hr/>
                         <li class="media">
                             <div class="media-left">
-                                <a href="#" title="Post">
-                                    <img class="media-object" src="img/annualevents/afterhours.jpg" alt="Post">
-                                </a>
+                                <img class="media-object" src="img/annualevents/afterhours.jpg" alt="Picture of After Hours">
                             </div>
                             <div class="media-body">
-                                <h3 class="media-heading"><a href="#" title="WIC AFTER HOURS">WIC AFTER HOURS</a></h3>
-                                <p>The Women in Computing After Hours Program is an overnight program for women accepted into the B. Thomas Golisano College of Computing. This free program includes learning about Study Abroad, Housing, Academic Support Services, Cooperative Education, leadership opportunities and much more.</p>                               
+                                <h3 class="media-heading">WiC After Hours</h3>
+                                <p>The Women in Computing After Hours Program is an overnight program for women accepted into the B. Thomas Golisano College of Computing. This free program includes learning about Study Abroad, Housing, Academic Support Services, Cooperative Education, leadership opportunities and much more. <a href="afterhours.php">Learn More...</a></p>                               
                             </div>
                         </li>
+                        <hr/>
                         <li class="media">
                             <div class="media-left">
-                                <a href="#" title="Post">
-                                    <img class="media-object" src="img/annualevents/wicconnects.jpg" alt="Post">
-                                </a>
+                                <img class="media-object" src="img/annualevents/wicconnects.jpg" alt="Picture of WiConnects">
                             </div>
                             <div class="media-body">
-                                <h3 class="media-heading"><a href="#" title="WICONNECTS">WICONNECTS</a></h3>
+                                <h3 class="media-heading">WiConnects</h3>
                                 <p>An evening event celebrating Golisano College female computing professionals and allies. The event is comprised of a keynote, tech talk, networking event, dinner and lasting words of inspiration.</p>                               
                             </div>
-                        </li>                     
+                        </li>
+                        <hr/>                     
                     </ul>                    
-                    <h2 class="category-title">CONFERENCES</h2>
+                    <h2 class="category-title" id="conferences">CONFERENCES</h2>
                     <ul class="media-list">
                         <li class="media">
                             <div class="media-left">
-                                <a href="#" title="Post">
-                                    <img class="media-object" src="img/annualevents/ghc.JPG" alt="Post">
-                                </a>
+                                <img class="media-object" src="img/annualevents/NYCWIC.jpg" alt="Picture of NYCWiC">
                             </div>
                             <div class="media-body">
-                                <h3 class="media-heading"><a href="#" title="Grace Hopper Women In Computing">Grace Hopper Celebration of Women in Computing</a></h3>
-                                <p>The Grace Hopper Celebration of Women in Computing is the World’s largest gathering of women technologists. It is designed to bring the research and career interests of women in computing to the forefront. Started in 1994, GHC is now the world’s largest gathering of women technologists. In 2013, GHC hosted over 4,750 attendees from 53 countries including 1,900 students from over 400 academic institutions and 2,850 professionals from industry, government and academia. The conference features keynotes by prominent women in technology, career workshops, and technical tracks from leading researchers and the best of ACM SIGSOFT and SIGGRAPH. Meet the women who are transforming technology at the Grace Hopper Celebration.</p>
+                                <h3 class="media-heading">New York Celebration of Women in Computing (NYCWiC)</h3>
+                                <p>New York Celebration of Women in Computing (NYCWiC) Conference is every two years in April. Attendees will be women in industry and academia from all over New York state. NYCWiC hopes to encourage women to complete their studies in computing by exploring careers and meeting women leaders from business, industry and academia. <a href="http://nycwic.hosting.acm.org/">Learn More...</a></p>
                             </div>
-                        </li> 
+                        </li>  
+                        <hr/>
                         <li class="media">
                             <div class="media-left">
-                                <a href="#" title="Post">
-                                    <img class="media-object" src="img/annualevents/NYCWIC.jpg" alt="Post">
-                                </a>
+                                <img class="media-object" src="# " alt="Picture of WiCyS">
                             </div>
                             <div class="media-body">
-                                <h3 class="media-heading"><a href="#" title="New York Celebration of Women in Computing (NYCWiC)">New York Celebration of Women in Computing (NYCWiC)</a></h3>
-                                <p>New York Celebration of Women in Computing (NYCWiC) Conference is every two years in April. Attendees will be women in industry and academia from all over New York state. NYCWiC hopes to encourage women to complete their studies in computing by exploring careers and meeting women leaders from business, industry and academia.</p>
+                                <h3 class="media-heading">Women in Cyber Security (WiCyS)</h3>
+                                <p>Women in Cyber Security (WiCyS) is a conference in March only for women in the Computer Security majors. WiCyS is an effort to build a community of women students/faculty/researchers/professionals in cybersecurity from academia, research and industry. <a href="https://www.wicys.net/">Learn More...</a></p>
                             </div>
-                        </li>                   
+                        </li>  
+                        <hr/>
+                        <li class="media">
+                            <div class="media-left">
+                                <img class="media-object" src="img/annualevents/ghc.JPG" alt="Picture of Grace Hopper Conference Group">
+                            </div>
+                            <div class="media-body">
+                                <h3 class="media-heading">Grace Hopper Celebration of Women in Computing</h3>
+                                <p>The Grace Hopper Celebration of Women in Computing is the World’s largest gathering of women technologists. It is designed to bring the research and career interests of women in computing to the forefront. Started in 1994, GHC is now the world’s largest gathering of women technologists. In 2013, GHC hosted over 4,750 attendees from 53 countries including 1,900 students from over 400 academic institutions and 2,850 professionals from industry, government and academia. The conference features keynotes by prominent women in technology, career workshops, and technical tracks from leading researchers and the best of ACM SIGSOFT and SIGGRAPH. Meet the women who are transforming technology at the Grace Hopper Celebration. <a href="https://ghc.anitab.org/">Learn More...</a></p>
+                            </div>
+                        </li>                 
                     </ul>                    
                 </section>
                 <aside class="sidebar col-sm-3">
