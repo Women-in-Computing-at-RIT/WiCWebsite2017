102a640b20e10b08d7885fd99e580c875726cdba
dkrsorenson <dkrsorenson@gmail.com>
2017-11-17 14:49:04 -0500

    Mobile updates

--- allies.php
+++ allies.php
@@ -40,9 +40,12 @@
                     <br><br>
                     
                     <h2 class="page-title">HOW TO BE AN ALLY</h2>
-
-                    <iframe id="slideshow" src="https://docs.google.com/presentation/d/e/2PACX-1vSRG_OvztNPDq1XNsZ1avpRvQ38jy6do9z-JnQYjM_MpHvJp2g0VDa82Ni9oY_7grLg-CUrzfTR5r5E/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
                     
+                    <iframe id="slideshow" src="https://docs.google.com/presentation/d/e/2PACX-1vSRG_OvztNPDq1XNsZ1avpRvQ38jy6do9z-JnQYjM_MpHvJp2g0VDa82Ni9oY_7grLg-CUrzfTR5r5E/embed?start=false&loop=false&delayms=3000" frameborder="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+                    
+                    <div class="entry">
+                        <a href="#" id="slides-link">Check us out!</a>
+                    </div>
                 </section>
                 <aside class="sidebar col-sm-3">
                     <div class="widget">
@@ -60,7 +63,5 @@
         </div>
     </main>
     <?php include 'footer.php';?>
-<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
-<!--    <script src="js/bootstrap.min.js"></script>-->
 </body>
 </html>
