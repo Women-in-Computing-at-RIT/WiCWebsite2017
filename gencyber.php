79de435682f969a5dadbf91ba2877c8feda5c63c
jessi <jcm9472@rit.edu>
2019-04-18 11:40:53 -0400

    gencyber page

---
+++ gencyber.php
@@ -0,0 +1,117 @@
+<?php $title="GenCyber"; $level="";?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'header.php';?>
+<body>
+    <div class="bread_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <ol class="breadcrumb">
+                        <li><a href="index.php">Home</a></li>
+                        <li class="active">GenCyber</li>
+                    </ol>                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <main class="site-main page-main">
+        <div class="container">
+            <div class="row">
+                <section class="page col-sm-9">
+                    <h2 class="page-title">GENCYBER GIRLS SUMMER CAMP</h2>
+                    <div class="entry">
+                        <center>
+                        <img src="img/gencyber/gencyberlogo.jpg">
+                        
+                        <h3>Meet new people and encounter new challenges!</h3>
+
+                        <p>With hacking stories appearing daily in global news headlines, and a world that's more
+                            interconnected than ever, we need sharp minds focused on safeguarding online communities
+                            and capabilities. Girls will love this all-girls hands-on camp that emphasizes cyber ethics,
+                            online safety, cyber security and defense.</p>
+                        
+                        <p>This RIT camp is funded by a federal grant from the NSA Gen-Cyber program and is
+                            <strong>free</strong> for accepted students to attend.</p>
+
+                        <br><img src = "/img/gencyber/gencyber2.JPG">
+
+                        </center>
+                        <br>
+                        <h4>Eligibility:</h4>
+                            <ul>
+                            <li>Middle/High school girls (incoming grades 8-11)</li>
+                            <li>This is a beginner camp, so no previous experience is necessary.</li>
+                            <li>Students will need to complete the application process to be considered for acceptance.</li>
+                            </ul>
+                        <p>For acceptance, each student needs to:</p>
+                            <ol>
+                            <li>Send a letter of application describing her background for this workshop and explain
+                            why this workshop would be meaningful, and </li>
+                            <li>Request a school STEM teacher to email a letter of recommendation
+                                with the student name in the Subject.</li>
+                            </ol>
+                        <center>
+                        <div style = "margin-top:3%">
+                            <div style = "float:left; width:50%; margin-top:10%">
+                                <h3>DATE: JULY 22-28, 2019</h3>
+                                <h3>WHERE: Rochester Institute of Technology</h3>
+                                <h3>COST: FREE</h3><br>
+                                <h4>Sign up to be contacted when applications are available.<br><br>
+                                <a href = "http://bit.ly/2019GenCyberGirlCamp">http://bit.ly/2019GenCyberGirlCamp</a>
+                                </h4>
+                            </div>
+                            <div style = "float:left; width:50%">
+                                <img src = "img/gencyber/gencyber1.JPG">
+                            </div>
+                        </div>
+                        </center>
+
+                        <h4>Curriculum:</h4>
+                        <p>
+                            A week long camp designed to introduce girls to the world of cybersecurity and
+                            other computing skills. Sessions focus on core principles and practices of cybersecurity,
+                            robotics and programming, as well as emphasizing cyber ethics, online safety, cybersecurity
+                            and defense.
+                        </p>
+
+                        <h4>Learner Outcomes:</h4>
+                        <ul>
+                            <li>Demonstrate basic knowledge of essential concepts and first principles in cybersecurity</li>
+                            <li>Demonstrate an understanding of correct and safe online behavior</li>
+                            <li>Demonstrate an understanding of current and potential cybersecurity and privacy issues in everyday life</li>
+                            <li>Demonstrate basic programming skills</li>
+                            </ul>
+
+                        <h4>FAQ</h4>
+                        <p>
+                            Q: Do I need to have taken a course in programming or computer science?<br>
+                            A: Nope! We design the camps to be from the ground up.<br><br>
+                            Q: When will I know if I got accepted?<br>
+                            A: We will let you know in June if you were accepted.
+                            We regret we cannot confirm places before this.<br><br>
+                            Q: Is the camp free?<br>
+                            A:  The camp is free thanks to our sponsors.<br><br>
+                            This is due in part to the camp addressing the advancement of women in cybersecurity.
+                            Our goal is to provide our local community with a needed starting point. As such,
+                            this public education is free to encourage high school aged females a chance at this
+                            opportunity, enabling all girls to participate, no matter the family income.
+                            In addition to an education students will be provided with real time contact and talks
+                            from professionals whom will be sharing their valuable experiences and advice.<br><br>
+                            Q: Will food be provided?<br>
+                            A:  Lunch for each day of the camp along with snacks are provided.<br><br>
+                            Q: Is lodging provided?<br>
+                            A: Lodging is not provided.<br><br>
+                            Q: I have more questions, who do I contact?<br>
+                            A:  Please contact the program director at gencyber@rit.edu if you have any questions.
+                        </p>
+
+                    </div>
+                    <br><br>
+                </section>
+            </div>
+        </div>
+    </main>
+     <?php include 'footer.php';?>
+</body>
+</html>
