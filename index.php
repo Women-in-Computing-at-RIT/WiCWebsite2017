46ea3db3157e26319b06748fd1fdb788018dcc74
jessi <jcm9472@rit.edu>
2019-07-30 15:58:26 -0400

    announcements display

--- index.php
+++ index.php
@@ -195,28 +195,45 @@ Jessi Miller '23
     </main>
     <?php include 'footer.php';?>
 
-    <script src="https://www.gstatic.com/firebasejs/6.3.1/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/6.3.3/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/6.3.3/firebase-database.js"></script>
     <script>
+
         // Your web app's Firebase configuration
-        var firebaseConfig = {
+        const firebaseConfig = {
             apiKey: "AIzaSyDWjDu6xik5sE2LFOTbVdF5q_IXKiYh-Vk",
             authDomain: "wic-announcements.firebaseapp.com",
             databaseURL: "https://wic-announcements.firebaseio.com",
             projectId: "wic-announcements",
-            storageBucket: "",
+            storageBucket: "wic-announcements.appspot.com",
             messagingSenderId: "625263131467",
             appId: "1:625263131467:web:aadfcf7d5b9c9c40"
         };
+
         // Initialize Firebase
         firebase.initializeApp(firebaseConfig);
 
         var ref = firebase.database().ref();
 
+
         function displayTest(){
-            //ref.on('value', function (snapshot) {
-            document.getElementById("announcements-content").innerHTML = "test2";
-            //});
+            ref.on("value", function (snapshot) {
+                var data = snapshot.val().masterSheet;
+                bigString = "";
+                for(i=0;i<3; i++){
+                    //length - i - 1 to put most recent at the top
+                    bigString += makeAnnouncement(data[data.length-i-1]);
+                }
+                document.getElementById("announcements-content").innerHTML = bigString;
+            });
         }
+        function makeAnnouncement(row){
+            bigString = "<h2>" + row.title + "</h2>";
+            bigString += "<h4>" + row.date + "</h4>";
+            bigString += "<p>" + row.body + "</p>";
+            return bigString;
+        }
+
         window.onload = displayTest();
     </script>
 
